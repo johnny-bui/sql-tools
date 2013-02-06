c3c0df01990bf1eab71f9d128143701e15e5aafc
verylazyboy <>
2013-02-06 15:46:18 +0100

    correct version nr

--- DDLGrammar/src/ddlgrammar/testfile.sql
+++ DDLGrammar/src/ddlgrammar/testfile.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS halloworld (
-	xxx varchar(40) not null unique,
-	yyyy integer
+CREATE TABLE IF NOT EXISTS `halloworld` (
+	`xxx` varchar(40) not null unique,
+	`yyyy` integer
 );
 
 CREATE TABLE IF NOT EXISTS h (
-	a bigint,
-	b integer,
-constraint yyyy primary key (a) ,
+	`a` bigint,
+	`b` integer,
+constraint `yyyy` primary key (a) ,
 constraint zzzz foreign key (a) references halloworld (xxx)
 );

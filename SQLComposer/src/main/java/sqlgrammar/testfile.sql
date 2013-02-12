ecf44e08b4d2b1772318cf6b1270a86ae3af0fa9
phucluoi <phucluoi@gmail.com>
2013-02-12 23:26:26 +0100

    port to maven

---
+++ SQLComposer/src/main/java/sqlgrammar/testfile.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS `halloworld` (
+	`xxx` varchar(40) not null unique,
+	`yyyy` integer
+);
+
+CREATE TABLE IF NOT EXISTS h (
+	`a` bigint,
+	`b` integer,
+constraint `yyyy` primary key (a) ,
+constraint zzzz foreign key (a) references halloworld (xxx)
+);
+
+DroP TaBle If Exists `xxxxxx`, `yyyyy`;
+
+insert into `xxxxxxx` (`a`,`b`,`c`, d) values (1, "xyzt", @xxxxx, 1.10000);
+insert into `xxxxxxx` (`a`,`b`,`c`, d) values 
+		(1, "xyzt", @xxxxx, 1.10000),
+		(1, "xyzt", @xxxxx, 1.10000),
+		(1, "xyzt", @xxxxx, 1.10000),
+		(1, "xyzt", @xxxxx, 1.10000),
+		(1, "xyzt", @xxxxx, 1.10000),
+		(1, "xyzt", @xxxxx, 1.10000),
+		(1, "xyzt", @xxxxx, 1.10000) ;

2d00aadd4edd99038d864c999b603a0797f41d32
verylazyboy <phucluoi@gmail.com>
2013-03-08 19:37:48 +0100

    zwichen BK

--- SQLChecker/src/main/java/sqlgrammar/SQLGrammar.java
+++ SQLChecker/src/main/java/sqlgrammar/SQLGrammar.java
@@ -9,10 +9,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PushbackReader;
 import java.io.StringReader;
-import mysqlgrm.lexer.Lexer;
-import mysqlgrm.lexer.LexerException;
-import mysqlgrm.parser.Parser;
-import mysqlgrm.parser.ParserException;
+//import mysqlgrm.lexer.Lexer;
+//import mysqlgrm.lexer.LexerException;
+//import mysqlgrm.parser.Parser;
+//import mysqlgrm.parser.ParserException;
 
 /**
  *
@@ -22,38 +22,38 @@ public class SQLGrammar {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Running .....");
-		try {
-			InputStream resourceAsStream = SQLGrammar.class.getResourceAsStream("testfile.sql");
-			Lexer l = new Lexer(new PushbackReader( new InputStreamReader(resourceAsStream) ));
-			Parser p = new Parser(l);
-			p.parse();
-		} catch (ParserException ex) {
-			System.err.println(ex.getMessage());
-			System.err.println(ex.getToken().getClass().getName());
-			System.err.println(ex.getToken().getText());
-		} catch (LexerException ex) {
-			System.err.println(ex.getMessage());
-			System.err.println(ex.getToken().getClass().getName());
-			System.err.println(ex.getToken().getText());
-		} catch (IOException ex) {
-			System.err.println(ex);
-			ex.printStackTrace();
-		}
-		System.out.println("RUN SUCCESS!!!");
+//		System.out.println("Running .....");
+//		try {
+//			InputStream resourceAsStream = SQLGrammar.class.getResourceAsStream("testfile.sql");
+//			Lexer l = new Lexer(new PushbackReader( new InputStreamReader(resourceAsStream) ));
+//			Parser p = new Parser(l);
+//			p.parse();
+//		} catch (ParserException ex) {
+//			System.err.println(ex.getMessage());
+//			System.err.println(ex.getToken().getClass().getName());
+//			System.err.println(ex.getToken().getText());
+//		} catch (LexerException ex) {
+//			System.err.println(ex.getMessage());
+//			System.err.println(ex.getToken().getClass().getName());
+//			System.err.println(ex.getToken().getText());
+//		} catch (IOException ex) {
+//			System.err.println(ex);
+//			ex.printStackTrace();
+//		}
+//		System.out.println("RUN SUCCESS!!!");
 	}
 
 	public static void checkSyntax(String sqlString) throws RuntimeException{
-		try {
-			Lexer l = new Lexer(new PushbackReader( new StringReader(sqlString) ));
-			Parser p = new Parser(l);
-			p.parse();
-		} catch (ParserException ex) {
-			throw new RuntimeException(ex.getMessage() + "\n" + sqlString, ex);
-		} catch (LexerException ex) {
-			throw new RuntimeException(ex.getMessage() + "\n" + sqlString , ex);
-		} catch (IOException ex) {
-			throw new RuntimeException(ex.getMessage() + "\n" + sqlString  , ex);
-		}
+//		try {
+//			Lexer l = new Lexer(new PushbackReader( new StringReader(sqlString) ));
+//			Parser p = new Parser(l);
+//			p.parse();
+//		} catch (ParserException ex) {
+//			throw new RuntimeException(ex.getMessage() + "\n" + sqlString, ex);
+//		} catch (LexerException ex) {
+//			throw new RuntimeException(ex.getMessage() + "\n" + sqlString , ex);
+//		} catch (IOException ex) {
+//			throw new RuntimeException(ex.getMessage() + "\n" + sqlString  , ex);
+//		}
 	}
 }
